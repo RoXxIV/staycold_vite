4d4635c48212e513c7e40642b78a5a8545bfd4e9
evan <evan.hermier@gmail.com>
2022-02-09 23:56:17 +0100

    ajout apollo + test

--- client/src/main.js
+++ client/src/main.js
@@ -4,9 +4,27 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:1337/graphql",
+});
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.provide(DefaultApolloClient, apolloClient);
 app.mount("#app");
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,6 +1,32 @@
-<script setup></script>
+<script setup>
+import { useQuery, useResult } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+const { result, loading, error } = useQuery(gql`
+  query getBaths {
+    baths {
+      data {
+        id
+        attributes {
+          waterTemperature
+          timeInWater
+          weather
+          date
+        }
+      }
+    }
+  }
+`);
+const baths = useResult(result, null, (data) => data.baths.data);
+</script>
 
 <template>
   <p>home</p>
+  <div v-if="loading">Loading...</div>
+  <div v-else-if="error">Error: {{ messages }}</div>
+  <ul v-else-if="result && result.baths">
+    <li v-for="bath of baths" :key="bath.id">
+      {{ bath.attributes.waterTemperature }}
+    </li>
+  </ul>
 </template>
 <style lang="scss"></style>
